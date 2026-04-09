5114817261ae2e4163261e56e7cd4c8b7336fe02
swimch <sven.rohan@bluewin.ch>
2026-04-09 19:03:51 +0200

    added programmer art & fixed API calls, #42

--- app/game/[gameCode]/players/[userId]/wizard/page.tsx
+++ app/game/[gameCode]/players/[userId]/wizard/page.tsx
@@ -5,27 +5,35 @@ import { useRouter, useParams } from "next/navigation";
 import { Button, App } from "antd";
 import { useApi } from "@/hooks/useApi";
 import useLocalStorage from "@/hooks/useLocalStorage";
+import attack from "./images/attack.png";
+import balanced from "./images/balanced.png";
+import tank from "./images/tank.png";
+import gambler from "./images/gambler.png";
 
 const WIZARDS = [
   {
     id: "ATTACKWIZARD",
     title: "Attack Wizard",
     description: "Unleashes devastating damage but has very low health.",
+    image: attack,
   },
   {
     id: "TANKWIZARD",
     title: "Tank Wizard",
     description: "With a massive health pool but reduced damage, they stand firm against any onslaught.",
+    image: tank,
   },
   {
     id: "BALANCEDWIZARD",
     title: "Balanced Wizard",
     description: "A perfect blend of offensive power and durability.",
+    image: balanced,
   },
   {
     id: "GAMBLERWIZARD",
     title: "Gambling Wizard",
     description: "With random multipliers and unstable health, every battle is a high-stakes risk.",
+    image: gambler,
   },
 ];
 
@@ -41,9 +49,9 @@ export default function Wizard() {
 
   const handleChooseWizard = async (gameCode: string, userId: number, wizardClass: string) => {
     try {
-      await apiService.put(`/games/${gameCode}/players/${userId}/wizard`, { wizardClass });
+      await apiService.put(`/game/${gameCode}/players/${userId}/wizard`, { wizardClass });
       message.success(`You have chosen the ${wizardClass} Wizard!`);
-      router.push(`/game/${gameCode}/attacks`);
+      router.push(`/game/${gameCode}/players/${userId}/attacks`);
     } catch (error) {
       message.error("Failed to choose wizard. Please try again.");
     }
@@ -60,11 +68,19 @@ export default function Wizard() {
           <div key={wizard.id} className={styles.wizard}>
             <h2 className={styles.wizardTitle}>{wizard.title}</h2>
             <Button
-              onClick={() => handleChooseWizard(gameCode, userId, wizard.id)}
-              block
-              className={styles.buttonWizard}
+            block
+            className={styles.buttonWizard}
+            style={{
+              width: 200,
+              height: 300,
+              padding: 0,
+              backgroundImage: `url(${wizard.image.src})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+            }}
+            onClick={() => handleChooseWizard(gameCode, userId, wizard.id)}
             >
-              Choose {wizard.title}
             </Button>
             <p className={styles.wizardDescription}>{wizard.description}</p>
           </div>
